27aadee92fc24a23ad07a3673f21a6d704990207
haragouri <uberhara@gmail.com>
2023-01-23 19:19:41 -0700

    text input and console

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");//hey browser, query this id #
-btnTranslate.addEventListener("click",clickHandler); //on that id, add a listner click, and if clicked, call function to show in console that it was clicked
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
 
 function clickHandler(){
     console.log("clicked");
+    console.log("input",txtInput.value);
 }
 
+btnTranslate.addEventListener("click",clickHandler); //on that id, add a listner click, and if clicked, call function to show in console that it was clicked
