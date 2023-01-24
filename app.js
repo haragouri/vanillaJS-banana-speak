db35d5c8d959d9fa30867a254c77fe26cd64fd7e
haragouri <uberhara@gmail.com>
2023-01-23 19:01:15 -0700

    querySelector&.addEventListener("click", callback)

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-var username = prompt("Give me username");
-var welcomeMesg = "This works" + username;
-console.log(welcomeMesg);
+var btnTranslate = document.querySelector("#btn-translate");//hey browser, query this id #
+btnTranslate.addEventListener("click",clickHandler); //on that id, add a listner click, and if clicked, call function to show in console that it was clicked
+
+function clickHandler(){
+    console.log("clicked");
+}
+
