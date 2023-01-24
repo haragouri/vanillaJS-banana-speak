81e36d2a52f9e49eb70b65bbb0162ff41b394ee7
haragouri <uberhara@gmail.com>
2023-01-24 10:57:25 -0700

    change mock APIs and then use PROD API

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");//hey browser, query this id #
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
 }
 
-function errorHandler(){
+function errorHandler(error){
     console.log("error occured", error);
     alert("something wrong with server");
 }
 
 function clickHandler(){
- // outputDiv.innerText = "afdgfdhdfhgdh" + txtInput.value ;
-
-
  var inputText = txtInput.value;
 
  fetch(getTranslationURL(inputText))
    .then(response => response.json())
-   .then(json => console.log(json))
+   .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+   })
    .catch(errorHandler)
-}
+};
 
 btnTranslate.addEventListener("click",clickHandler); //on that id, add a listner click, and if clicked, call function to show in console that it was clicked
+
